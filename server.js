4680e0d141cbce7acf9c324b76f79ddb4c1dcb26
satish-313 <pradhansatish53@gmail.com>
2021-03-25 17:29:18 +0530

    elastic beanstack upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app = express();
 
 port = process.env.PORT || 4000;
 app.get("/", (req,res) => {
-    res.send("hello world from ec2")
+    res.send("hello world from ec2, it failed. Try in elastic beanstack")
 })
 
 app.listen(port, () => console.log("server is running on port: ", port))
